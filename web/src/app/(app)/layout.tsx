e730178e73439d02420574c567d238bd9c84b9a9
wesho1107 <wesho1107@gmail.com>
2026-05-18 15:08:50 +0800

    chore: accidentally deleted lol

--- web/src/app/(app)/layout.tsx
+++ web/src/app/(app)/layout.tsx
@@ -26,8 +26,6 @@ function AppLoadingSkeleton() {
           <Skeleton className="h-32 rounded-xl" />
           <Skeleton className="h-32 rounded-xl" />
           <Skeleton className="h-32 rounded-xl" />
-          App loading skeleton
-          
         </div>
       </main>
     </div>
